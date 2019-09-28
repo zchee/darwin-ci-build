b7049b8950ec5a413133b07f9ac05ad4cc62434b
Koichi Shiraishi <zchee.io@gmail.com>
2019-09-29 01:11:48 +0900

    scripts: fix arg check

--- scripts/codesign.bash
+++ scripts/codesign.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-if [[ -z $CMD_NAME ]]; then
+if [[ -z $1 ]]; then
   echo "Usage: $0 [codesign name]"
   exit 1
 fi
@@ -17,7 +17,7 @@ fi
 
 cat <<EOF > "$CODESIGN_NAME".cfg
 [ req ]
-default_bits            = 2048                  # RSA key size
+default_bits            = 4096                  # RSA key size
 encrypt_key             = no                    # Protect private key
 default_md              = sha512                # MD to use
 prompt                  = no                    # Prompt for DN
@@ -29,7 +29,7 @@ keyUsage                = critical,digitalSignature
 extendedKeyUsage        = critical,codeSigning
 EOF
 
-openssl req -new -newkey rsa:2048 -x509 -days 3650 -nodes -config "$CODESIGN_NAME.cfg" -extensions codesign_reqext -batch -out "$CODESIGN_NAME.cer" -keyout "$CODESIGN_NAME.key"
+openssl req -new -newkey rsa:4096 -x509 -days 7300 -nodes -config "$CODESIGN_NAME.cfg" -extensions codesign_reqext -batch -out "$CODESIGN_NAME.cer" -keyout "$CODESIGN_NAME.key"
 sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain "$CODESIGN_NAME.cer"
 sudo security import "$CODESIGN_NAME.key" -A -k /Library/Keychains/System.keychain
 
